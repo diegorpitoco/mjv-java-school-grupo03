4dbf49864ecf6afc44face646028818d5071fe10
Alane-Kerolaine <alanekerolaine@live.com>
2023-06-09 15:11:24 -0300

    criando o atributo numeroAgencia e o método transferirParaOutraConta

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,7 +1,13 @@
 class ContaCorrente{
     int numConta;
+    int numeroAgencia;
 
     public void sacar(double valor){
         saldo = saldo - valor;
     }
+
+    public double transferirParaOutraConta(double valorTransferencia){
+        return saldo -= valorTransferencia;
+    }
+
 }
