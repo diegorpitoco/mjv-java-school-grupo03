0167ca91fb4ea1999c326aca2eadd558825df5b7
diegorpitoco <diegorpitoco@gmail.com>
2023-06-10 22:31:38 -0300

    Criando a classe ContaCorrente e a sua estrutura

---
+++ ContaCorrente.java
@@ -0,0 +1,26 @@
+import java.sql.Date;
+
+public class ContaCorrente {
+
+    int numeroConta;
+    int numeroAgencia;
+    String nomeCliente;
+    Date dataNascimento;
+    Double saldo;
+
+    public void sacar() {
+    }
+
+    public void transferirParaOutraConta() {
+    }
+
+    public void cancelarContaJustificativa() {
+    }
+
+    public void consultarExtratoEntreDuasDatas() {
+    }
+
+    public void consultarSaltoAtual() {
+    }
+
+}
