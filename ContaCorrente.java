6e1f7e99926491855e7d3933d6c152a5b4806ffb
Estefania Carrer <estefania.carrer@hotmail.com>
2023-06-11 15:53:19 -0300

    Atualizando métodos

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -8,19 +8,29 @@ public class ContaCorrente {
     Date dataNascimento;
     Double saldo;
 
-    public void sacar() {
+    public void sacar(double valor){
+        saldo = saldo - valor;
     }
 
-    public void transferirParaOutraConta() {
+    public double transferirParaOutraConta(int contaDestino, double valorTransferencia){
+        return saldo -= valorTransferencia;
     }
 
-    public void cancelarContaJustificativa() {
+    public void cancelarContaJustificativa(String justificativa) {
+        if (saldo == 0.0) {
+            System.out.println("Conta cancelada com sucesso. Justificativa: " + justificativa);
+        } else {
+            System.out.println("Não foi possível cancelar a conta. Saldo não zerado.");
+        }
     }
 
-    public void consultarExtratoEntreDuasDatas() {
+    public void consultarExtratoEntreDatas(Date dataInicio, Date dataFim) {
+        System.out.println("Extrato entre as datas " + dataInicio + " e " + dataFim);
+        // Implementar lógica para consulta.
     }
 
-    public void consultarSaltoAtual() {
+    public void consultarSaltoAtual(double saldo) {
+        System.out.println("Saldo atual: R$" + saldo);
     }
 
 }
